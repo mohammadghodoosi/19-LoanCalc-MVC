fb478e16d135eb1ce0c82798cbcf4301f9f846fa
Mohammad Ghodoosi <ghodusimohamad@gmail.com>
2021-09-01 14:30:25 +0430

    commit 4
    add business logic to the app
    create a new folder
    add public function, to it in order to bring data from a place
    add private function, to avoid spaghetti coding

--- LoanCalc/Models/LoanPayment.cs
+++ LoanCalc/Models/LoanPayment.cs
@@ -7,5 +7,18 @@ namespace LoanCalc.Models
 {
     public class LoanPayment
     {
+        //the number of month
+        public int Month { get; set; }
+
+        //the payment that was payed
+        public decimal Payment { get; set; }
+
+
+        public decimal MonthlyPrincipal { get; set; }
+        public decimal MonthlyInterest { get; set; }
+        public decimal TotalIntrest { get; set; }
+
+        //whats left
+        public decimal Ballance { get; set; }
     }
 }
